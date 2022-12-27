af0a6d1ec89d14cf14a32a2483eca81216400afb
Nikita <khakimov.nikita.vch@gmail.com>
2022-12-27 23:37:02 +0200

    hookDebounce

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,14 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useDebounce } from '../hooks/debounce';
 import { useSearchUsersQuery } from '../store/github/github.api'
 
 const HomePage = () => {
 
+	const [search, setSearch] = useState('');
 	const { data, isLoading, isError } = useSearchUsersQuery('rona');
+	const debounced = useDebounce(search);
 
-	console.log(data)
+	useEffect(() => {
+		console.log(search)
+	}, [debounced])
 
 	return (
-		<div>HomePage</div>
+		<div className='flex justify-center pt-10 mx-auto h-screen w-screen'>
+			{isError && <p className='text-center text-red-600'>Something went wrong...</p>}
+
+			<div className='relative w-[560px]'>
+				<input 
+					type="text" 
+					className='border py-2 px-4 w-full h-[42px] mb-2' 
+					placeholder='Search for Github username...'
+					value={search}
+					onChange={(e) => setSearch(e.target.value)}
+				/>
+				<div className='absolute top-[42px] left-0 right-0 max-h-[200px] shadow-md bg-white'>
+					Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim, nam?
+				</div>
+			</div>
+		</div>
 	)
 }
 
