bc06f9390dec4121a86fb5beccef042e54a8fe5b
Nikita <khakimov.nikita.vch@gmail.com>
2022-12-27 22:46:35 +0200

    getUsersGithubApi

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import {Routes, Route} from 'react-router-dom'
+import Navigation from './components/Navigation';
 import FavouritesPage from './pages/FavouritesPage';
 import HomePage from './pages/HomePage';
 
 function App() {
   return (
+		<>
+		<Navigation />
+	
 		<Routes>
 			<Route path='/' element={<HomePage />}/>
 			<Route path='/favourites' element={<FavouritesPage />}/>
 		</Routes>
+		</>
+		
   );
 }
 
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,12 @@
 import React from 'react'
+import { useSearchUsersQuery } from '../store/github/github.api'
 
 const HomePage = () => {
+
+	const { data, isLoading, isError } = useSearchUsersQuery('rona');
+
+	console.log(data)
+
 	return (
 		<div>HomePage</div>
 	)
